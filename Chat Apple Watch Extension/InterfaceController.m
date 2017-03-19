81556970c8d6399bd55f16f2625bbfdf73cb5876
Neil Nie <yongyang.nie@gmail.com>
2017-03-19 10:58:54 -0400

    Pods conflicts resolved, watch extension added

---
+++ Chat Apple Watch Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Chat Apple Watch Extension
+//
+//  Created by Yongyang Nie on 3/19/17.
+//  Copyright © 2017 letsbuildthatapp. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController ()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
---
+++ Chat Apple Watch Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Chat Apple Watch Extension
+//
+//  Created by Yongyang Nie on 3/19/17.
+//  Copyright © 2017 letsbuildthatapp. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
